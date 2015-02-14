cd80158bb2371c7eca99ad750b299db10e4c2342
beta_chelsea <beta.chelsea@gmail.com>
2015-02-14 16:16:43 +0900

    xmlパースのため、xml2js 導入

--- routes/onsenInfo.js
+++ routes/onsenInfo.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var config = require('config');
+var parseString = require('xml2js').parseString;
 
 /* 温泉データ取得 from じゃらん */
 router.get('/', function(req, res, next) {
@@ -10,10 +11,12 @@ router.get('/', function(req, res, next) {
     url: 'http://jws.jalan.net/APICommon/OnsenSearch/V1/?key=' + config.get('jalan-api-key') + '&l_area=010300&count=1&xml_ptn=1'
   };
 
-  var callback = function(err, api_res, body) {
+  var callback = function(err, api_res, xml_body) {
     if (!err && api_res.statusCode === 200) {
-      console.log(body);
-      res.send('respond with a resource');
+      parseString(xml_body, function(err, result) {
+        console.log(JSON.stringify(result));
+        res.send('respond with a resource');
+      });
     }
   };
   request.post(options, callback);
