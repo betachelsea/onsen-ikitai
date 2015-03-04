dbc213644a8680a5ca7e7b415e60e4e91f6912a2
beta_chelsea <beta.chelsea@gmail.com>
2015-03-05 08:24:13 +0900

    金沢駅付近の温泉情報を取得するよう実装

--- routes/onsenInfo.js
+++ routes/onsenInfo.js
@@ -4,11 +4,44 @@ var request = require('request');
 var config = require('config');
 var parseString = require('xml2js').parseString;
 
-/* 温泉データ取得 from じゃらん */
+var YAHOO_GEOCODER_API = 'http://geo.search.olp.yahooapis.jp/OpenLocalPlatform/V1/geoCoder?appid=' + config.get('yahoo-api-key');
+var JALAN_AREA_SEARCH = 'http://jws.jalan.net/APICommon/AreaSearch/V1/?key=' + config.get('jalan-api-key');
+
+// TODO: Refactoring
+/* 温泉データ取得 */
 router.get('/', function(req, res, next) {
+  var jalanAreaHash = []; // jalanAreaHash['北海道'] = '010000';
+  var targetPrefName = '東京都'; // initialize
+  request.post({ url: JALAN_AREA_SEARCH }, function(err, api_res, xml_body) {
+    parseString(xml_body, function(err, result) {
+      var areaJson = result.Results.Area[0].Region;
+      for (var i=0; i<areaJson.length; i++) {
+        for (var j=0; j<areaJson[i].Prefecture.length; j++) {
+          var pref = areaJson[i].Prefecture[j].$;
+          jalanAreaHash[pref.name] = pref.cd;
+        }
+      }
+      request.post(options2, callback2);
+    });
+  });
+
+  var options2 = {
+    url: YAHOO_GEOCODER_API + '&lat=36.578273&lon=136.647763' // 金沢駅
+  };
+
+  var callback2 = function(err, api_res, xml_body) {
+    if (!err && api_res.statusCode === 200) {
+      parseString(xml_body, function(err, result) {
+        var prefName = result.YDF.Feature[0].Name[0].match(/.+都|.+道|.+府|.+県/)[0]; // 石川県
+        if (prefName !== undefined) targetPrefName = prefName;
+        console.log(targetPrefName);
+        request.post(options, callback);
+      });
+    }
+  };
 
   var options = {
-    url: 'http://jws.jalan.net/APICommon/OnsenSearch/V1/?key=' + config.get('jalan-api-key') + '&l_area=010300&count=1&xml_ptn=1'
+    url: 'http://jws.jalan.net/APICommon/OnsenSearch/V1/?key=' + config.get('jalan-api-key') + '&pref=190000'
   };
 
   var callback = function(err, api_res, xml_body) {
@@ -16,9 +49,12 @@ router.get('/', function(req, res, next) {
       parseString(xml_body, function(err, result) {
         res.render('onsen_info', {onsen: result.Results.Onsen[0]});
       });
+      return true;
     }
+    console.log(api_res);
+    res.send("error at じゃらんAPI");
   };
-  request.post(options, callback);
+
 });
 
 module.exports = router;
