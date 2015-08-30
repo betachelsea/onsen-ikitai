3c92dbb9f5e32833ced23641182bf88cc4f9156b
beta_chelsea <beta.chelsea@gmail.com>
2015-08-30 23:14:29 +0900

    Replace "double quote" -> 'single quote'

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 // http://qiita.com/teitei_tk/items/6a9833e4308b3769811d
-var gulp = require("gulp"),
-    del = require("del"),
+var gulp = require('gulp'),
+    del = require('del'),
     coffee = require('gulp-coffee'),
     uglify = require('gulp-uglify'),
-    sass = require("gulp-sass"),
+    sass = require('gulp-sass'),
     minifyCss = require('gulp-minify-css'),
     concat = require('gulp-concat'),
     server = require('gulp-express'),
@@ -19,37 +19,37 @@ gulp.task('watch', function() {
   gulp.watch(['./public/main.js', './public/style.css'], ['server']);
 });
 
-gulp.task("clean-public", function() {
+gulp.task('clean-public', function() {
   del(['./public/style.css', './public/main.js']);
 });
 
-gulp.task("compile-coffee", function() {
+gulp.task('compile-coffee', function() {
   del(['./source/dest/js/**/*.js']);
-  gulp.src("source/coffee/**/*.coffee")
+  gulp.src('source/coffee/**/*.coffee')
       .pipe(coffee())
       .pipe(gulp.dest('./source/dest/js'));
 });
 
-gulp.task("concat-js", function() {
-  gulp.src(["./source/dest/js/**/*.js"])
-      .pipe(concat("main.js"))
+gulp.task('concat-js', function() {
+  gulp.src(['./source/dest/js/**/*.js'])
+      .pipe(concat('main.js'))
       .pipe(uglify({preserveComments: 'some'}))
       .pipe(gulp.dest('public/'));
 });
 
-gulp.task("compile-sass", function() {
+gulp.task('compile-sass', function() {
   del(['./source/dest/css/**/*.css']);
-  gulp.src("source/sass/**/*.scss")
+  gulp.src('source/sass/**/*.scss')
       .pipe(sass())
       .pipe(gulp.dest('./source/dest/css'));
 });
 
-gulp.task("concat-css", function() {
-  gulp.src(["./source/dest/css/**/*.css"])
-      .pipe(concat("style.css"))
+gulp.task('concat-css', function() {
+  gulp.src(['./source/dest/css/**/*.css'])
+      .pipe(concat('style.css'))
       .pipe(minifyCss())
       .pipe(gulp.dest('public/'));
 });
 
-gulp.task("compile", ['clean-public', 'compile-coffee', 'compile-sass', 'concat-js', 'concat-css']);
-gulp.task("default", ['compile', 'server', 'watch']);
+gulp.task('compile', ['clean-public', 'compile-coffee', 'compile-sass', 'concat-js', 'concat-css']);
+gulp.task('default', ['compile', 'server', 'watch']);
